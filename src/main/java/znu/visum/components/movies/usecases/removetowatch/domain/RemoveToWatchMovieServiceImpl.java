9d416e8ff5de5a3b2648d733cef77df2d1554368
znuznu <znu_1@protonmail.com>
2022-01-30 14:45:50 +0100

    feat: DELETE /movies/:id/watchlist

---
+++ src/main/java/znu/visum/components/movies/usecases/removetowatch/domain/RemoveToWatchMovieServiceImpl.java
@@ -0,0 +1,34 @@
+package znu.visum.components.movies.usecases.removetowatch.domain;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import znu.visum.components.movies.domain.errors.NoSuchMovieIdException;
+import znu.visum.components.movies.domain.models.Movie;
+import znu.visum.components.movies.domain.ports.MovieRepository;
+
+@Service
+public class RemoveToWatchMovieServiceImpl implements RemoveToWatchMovieService {
+  private final MovieRepository movieRepository;
+
+  @Autowired
+  public RemoveToWatchMovieServiceImpl(MovieRepository movieRepository) {
+    this.movieRepository = movieRepository;
+  }
+
+  @Override
+  public boolean removeToWatch(long movieId) {
+    Movie movie =
+        movieRepository
+            .findById(movieId)
+            .orElseThrow(() -> new NoSuchMovieIdException(Long.toString(movieId)));
+
+    if (movie.isToWatch()) {
+      movie.setToWatch(false);
+      movieRepository.save(movie);
+
+      return true;
+    }
+
+    return false;
+  }
+}
